9fb287622a06b571afde655803d6a32b82588a18
trox115 <tofernandes67@gmail.com>
2020-06-17 15:56:49 +0100

    solution working now

--- graphs/clonegraph.js
+++ graphs/clonegraph.js
@@ -1,27 +1,44 @@
+/**
+ * // Definition for a Node.
+ * function Node(val, neighbors) {
+ *    this.val = val === undefined ? 0 : val;
+ *    this.neighbors = neighbors === undefined ? [] : neighbors;
+ * };
+ */
+
+/**
+ * @param {Node} node
+ * @return {Node}
+ */
 var cloneGraph = function(node) {
     let queue = [node]
+    let alreadyVisited = []
     let hashMap = {}
-    let i= 0
-    while (queue.length >0){
-      
-        let currentNode = queue.shift()
+    console.log(node)
+    if (node == null) return null;
+    while (queue.length > 0){
+        
+       let currentNode=queue.shift();
         if(currentNode.val in hashMap == false){
-            hashMap[currentNode.val] = new Node(currentNode.val,[])
+                        
+            
+          hashMap[currentNode.val] = new Node(currentNode.val,[])
         }
+        currentNode.neighbors.forEach(nodebor => {
+
+           if(nodebor.val in hashMap == false){
+               hashMap[nodebor.val] = new Node(nodebor.val,[])
+           }  
+            hashMap[currentNode.val].neighbors.push(hashMap[nodebor.val])
+         })
+        alreadyVisited.push(currentNode)
         
-       currentNode.neighbors.forEach(function(nodeNeig){ 
-           hashMap[nodeNeig.val]=new Node(nodeNeig.val,[])  
-            
-           hashMap[currentNode.val].neighbors.push(hashMap[nodeNeig.val])
-           
-           if (nodeNeig.val in hashMap ==false){
-           queue.push(nodeNeig)
-               }
-            });
-            
-        
+        currentNode.neighbors.some(node =>{
+           if(alreadyVisited.indexOf(node) < 0 && queue.indexOf(node)< 0){
+               queue.push(node)
+           } 
+        } )
         
     }
-    console.log(hashMap)
-    return hashMap[node.val]
+    return hashMap[node.val]    
 };
