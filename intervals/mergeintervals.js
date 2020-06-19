cf6fabe4f0d080869c6be0de1632de54b1678437
trox115 <tofernandes67@gmail.com>
2020-06-19 11:56:15 +0100

    Merge Intervals

---
+++ intervals/mergeintervals.js
@@ -0,0 +1,35 @@
+  /*
+    sort ([1,3],[2,6])
+    stack=[[1,3]]
+    
+    for(i=1;)
+    top = stack[stack-length-1]
+    
+    
+    */
+    
+    let stack =[]
+    let top = null
+    if(intervals.length <=1){
+        return intervals
+    }
+    intervals = intervals.sort((a,b) => a[0]-b[0])
+    console.log(intervals)
+    stack.push(intervals[0])
+    
+    for(let i=1; i<intervals.length;i+=1){
+        
+        top = stack[stack.length -1]
+        
+        if(top[1]<intervals[i][0]){
+            stack.push(intervals[i])
+        }
+        else if(top[1] < intervals[i][1]){
+            top[1] = intervals[i][1]
+            stack.pop();
+            stack.push(top)
+        }
+
+    }
+    return stack;
+}
